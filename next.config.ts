89a25cc9cc1519c34af196b4f24d9943bb2069ad
Kla Haeck <kla@zsh.email>
2025-06-26 13:00:12 -0500

    redirects

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  async redirects() {
+    return [
+      {
+        source: '/samchadha',
+        destination: '/?name=Sam%20Chadha',
+        permanent: false, // 308 redirect, use false for 307 temporary redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
