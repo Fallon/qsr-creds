f2b37c5ebcaa06803f29c09b77122008ed479ad6
Kla Haeck <kla@zsh.email>
2025-06-26 13:18:03 -0500

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import type { Viewport } from "next";
 
 export const viewport: Viewport = {
   themeColor: 'black',
-  width: '1024px',
+  width: '1024',
   // initialScale: 1,
   // maximumScale: 1,
   // minimumScale: 1,
