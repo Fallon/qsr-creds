1629d87e3a047fec6ea18cb534ded9fe49bfcd72
Kla Haeck <kla@zsh.email>
2025-07-07 09:26:56 -0500

    marcos update

--- src/app/marcospizza/page.client.tsx
+++ src/app/marcospizza/page.client.tsx
@@ -401,7 +401,7 @@ function HomeClientContent() {
 
         <section id="reel">
           <div className="aspect-video relative">
-            <iframe className="absolute top-0 left-0 w-full h-full border-0" src="https://player.vimeo.com/video/1097696716?h=d7f676fc2d&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media" title="Fallon Reel 2025"></iframe>
+            <iframe className="absolute top-0 left-0 w-full h-full border-0" src="https://player.vimeo.com/video/1099368525?h=d7f676fc2d&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media" title="Fallon Reel 2025"></iframe>
           </div>
         </section>
 
