fb53038ed7c4e85040a109bf41cd43dc35b8aa02
Kla Haeck <kla@zsh.email>
2025-08-04 13:41:54 -0500

    header exclamation point dynamic

--- src/components/SectionHelloDefault/index.tsx
+++ src/components/SectionHelloDefault/index.tsx
@@ -5,7 +5,7 @@ const SectionHelloDefault = ({ name }: { name: string }) => {
     <section id="hello" className="min-h-screen bg-[url('/hello-bg.png')] bg-cover bg-center text-foreground flex items-center justify-center relative">
       <div className="flex flex-col w-90 absolute left-10 text-[#faf0e0]">
         <h2 className="text-6xl font-family-anton uppercase text-[#faf0e0] tracking-wider pb-3">
-          <span className="flex gap-2">Hi there! <Image src={'asterisk.svg'} width={54} height={54} alt="asterisk" /></span>{name ? `${name}!` : ''}
+          <span className="flex gap-2">Hi there{name && name !== '' ? '': '!'} <Image src={'asterisk.svg'} width={54} height={54} alt="asterisk" /></span>{name ? `${name}!` : ''}
         </h2>
 
         <p className="font-family-garamond pb-3 leading-5">Not sure if you heard, but after more than a decade, Fallon and Arby’s are parting ways. We couldn’t be prouder of the work we’ve done and how we changed their business. Now we’re looking to change someone else’s. So take a look at what we do and if you’re looking to shake up QSR, give us a call.</p>
